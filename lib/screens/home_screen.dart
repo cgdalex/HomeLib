00bae40247ad2397565e5db7814ca83dd680f8fd
Ian Castaneda <iancastaneda713@gmail.com>
2026-05-02 17:26:03 -0700

    Track search input on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  void _searchBooks() {
+    print('Searching for: ${_searchController.text}');
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,20 +30,20 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(12),
         child: Row(
           children: [
-            const Expanded(
+            Expanded(
               child: TextField(
-                decoration: InputDecoration(
+                controller: _searchController,
+                decoration: const InputDecoration(
                   labelText: 'Search for a book',
                   hintText: 'Example: The Hobbit',
                   border: OutlineInputBorder(),
                 ),
+                onSubmitted: (_) => _searchBooks(),
               ),
             ),
             const SizedBox(width: 8),
             ElevatedButton(
-              onPressed: () {
-                print('Search button pressed');
-              },
+              onPressed: _searchBooks,
               child: const Text('Search'),
             ),
           ],
